faa3b75f68156583af6fa44c3e3a2c24a88f3057
Gabriel Cecconi <gabriel.cecconi25@gmail.com>
2024-09-18 23:11:04 -0300

    Arrumando erro de digitação do cpf

--- script/cadastro.js
+++ script/cadastro.js
@@ -10,7 +10,7 @@ async function cadastro(){
         let terms = document.getElementById('termos').checked ? 1 : 0;
 
         // Checagem de campos
-        if (!name || !email || !cpf_cpnj || !password || !data) {
+        if (!name || !email || !cpf_cnpj || !password || !data) {
             alert('Todos os campos são obrigatórios!\nPor favor, preencha todos os campos');
             return;
         }
