e9fd298f85c53a59c27956fbae685061648317c4
Gabriel Cecconi <gabriel.cecconi25@gmail.com>
2024-09-25 10:10:30 -0300

    Fixing errors in js login, adding quality of life changes to login script

--- script/login.js
+++ script/login.js
@@ -1,33 +1,43 @@
 const url = "https://go-wash-api.onrender.com/api/login"
 
 async function login(){
-    let user = document.getElementById('user').value;
-    let password = document.getElementById('password').value;
+    const loginButton = document.getElementById('login');
+    loginButton.disabled = true;
+    try {
+        let user = document.getElementById('user').value;
+        let password = document.getElementById('password').value;
 
-    if (!user || !password){
-        alert('Todos os campos precisam ser preenchidos!');
-        return;
-    };
+        if (!user || !password){
+            alert('Todos os campos precisam ser preenchidos!');
+            return;
+        };
 
-    let responseApi = await fetch(url, {
-        method:"POST",
-        body:JSON.stringify({
-            "email": user,
-            "password": password,
-            "user_type_id": 1
-        }),
-        headers:{
-            'Content-Type':'application/json'
-        }
-    });
+        let responseApi = await fetch(url, {
+            method:"POST",
+            body:JSON.stringify({
+                "email": user,
+                "password": password,
+                "user_type_id": 1
+            }),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
 
-    if (responseApi.ok) {
-        // No momento vou manter o data, mesmo não usando ele
-        let data = await responseApi.json();
-        alert('Login feito com sucesso');
-        window.location.replace("../index.html");
-    } else{
-        let errorData = await responseApi.json();
-        alert(errorData.data)
-    };
+        if (responseApi.ok) {
+            // No momento vou manter o data, mesmo não usando ele
+            let data = await responseApi.json();
+            alert('Login feito com sucesso');
+            window.location.replace("../index.html");
+        } else{
+            let errorData = await responseApi.json();
+            console.log(errorData)
+            let errorMessage = errorData.data.errors;
+            alert(errorMessage)
+        };
+    } catch(error){
+        console.log("Erro na requisição:", error)
+        alert("Erro incesperado. Tente novamente mais tarde.")
+    }
+    loginButton.disabled = false;
 }
